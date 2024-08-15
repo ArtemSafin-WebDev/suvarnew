d4a8ff7e2360ab05c84e2d43387831b7bdbefd08
Artem Safin <artemsafin.webdev@gmail.com>
2024-08-15 08:40:28 +0500

    По видеомодалке

--- src/js/aboutProject.ts
+++ src/js/aboutProject.ts
@@ -1,3 +1,6 @@
+import Plyr from "plyr";
+import "plyr/dist/plyr.css";
+
 export default function aboutProject() {
   const elements = Array.from(
     document.querySelectorAll<HTMLElement>(".about-project")
@@ -10,5 +13,38 @@ export default function aboutProject() {
       event.preventDefault();
       toggle.parentElement?.classList.toggle("content-shown");
     });
+
+    const modal = element.querySelector<HTMLElement>(".about-project__modal");
+
+    const video = element.querySelector<HTMLVideoElement>("video");
+    const videoLink = element.querySelector<HTMLLinkElement>(
+      ".about-project__video-wrapper"
+    );
+    const closeModal = element.querySelector<HTMLElement>(
+      ".about-project__modal-close"
+    );
+    if (video) {
+      const instance = new Plyr(video);
+
+      videoLink?.addEventListener("click", (event) => {
+        event.preventDefault();
+        document.body.classList.add("modal-open");
+        modal?.classList.add("active");
+        instance.play();
+      });
+      closeModal?.addEventListener("click", (event) => {
+        event.preventDefault();
+        document.body.classList.remove("modal-open");
+        modal?.classList.remove("active");
+        instance.stop();
+      });
+      modal?.addEventListener("click", (event) => {
+        if (event.target === modal) {
+          document.body.classList.remove("modal-open");
+          modal?.classList.remove("active");
+          instance.stop();
+        }
+      });
+    }
   });
 }
