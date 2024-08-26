2cca44793a8efc46b88c1e884fe6152364448893
Artem Safin <artemsafin.webdev@gmail.com>
2024-08-26 03:06:27 +0300

    По айфрейму

--- src/js/aboutProject.ts
+++ src/js/aboutProject.ts
@@ -23,28 +23,41 @@ export default function aboutProject() {
     const closeModal = element.querySelector<HTMLElement>(
       ".about-project__modal-close"
     );
-    if (video) {
-      const instance = new Plyr(video);
 
-      videoLink?.addEventListener("click", (event) => {
-        event.preventDefault();
-        document.body.classList.add("modal-open");
-        modal?.classList.add("active");
-        instance.play();
-      });
-      closeModal?.addEventListener("click", (event) => {
-        event.preventDefault();
+    let instance: Plyr | null = null;
+    if (video) {
+      instance = new Plyr(video);
+    }
+
+    videoLink?.addEventListener("click", (event) => {
+      event.preventDefault();
+      document.body.classList.add("modal-open");
+      modal?.classList.add("active");
+      instance?.play();
+    });
+    closeModal?.addEventListener("click", (event) => {
+      event.preventDefault();
+      document.body.classList.remove("modal-open");
+      modal?.classList.remove("active");
+      instance?.stop();
+      const iframe = modal?.querySelector("iframe");
+
+      if (iframe) {
+        let iframeSrc = iframe.src;
+        iframe.src = iframeSrc;
+      }
+    });
+    modal?.addEventListener("click", (event) => {
+      if (event.target === modal) {
         document.body.classList.remove("modal-open");
         modal?.classList.remove("active");
-        instance.stop();
-      });
-      modal?.addEventListener("click", (event) => {
-        if (event.target === modal) {
-          document.body.classList.remove("modal-open");
-          modal?.classList.remove("active");
-          instance.stop();
+        instance?.stop();
+        const iframe = modal?.querySelector("iframe");
+        if (iframe) {
+          let iframeSrc = iframe.src;
+          iframe.src = iframeSrc;
         }
-      });
-    }
+      }
+    });
   });
 }
